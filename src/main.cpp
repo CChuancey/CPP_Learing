dc79e9a58764ca92df3c382f33c7914f692f235e
Chuancey <37216768+MiChuancey@users.noreply.github.com>
2021-09-13 10:15:36 +0800

    ✨ feat: 用template写二叉树

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,59 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
-int main(int argc,char** argv){
+template<typename valType>
+class BTnode{
+    public:
 
-    cout << "hello !" << endl;
+    private:
+    valType _val;
+    int _cnt;
+    BTnode* _lchild;
+    BTnode* _rchild;
+};
+
+template <typename elemType>
+class BinaryTree{
+    public:
+    BinaryTree();
+    BinaryTree(const BinaryTree&);
+    ~BinaryTree();
+    BinaryTree& operator=(const BinaryTree&);
+
+    bool empty(){
+        return _root==0;
+    }
+    void clear();
+    private:
+    BTnode<elemType>* _root;
+
+    void copy(BTnode<elemType>* dst,BTnode<elemType>* src);
+};
+
+template<typename elemType>
+inline BinaryTree<elemType>::BinaryTree(const BinaryTree& rhs){
+    copy(_root,rhs._root);
+}
+
+template<typename elemType>
+inline BinaryTree<elemType>::~BinaryTree(){
+    clear();
+}
+
+template<typename elemType>
+inline BinaryTree<elemType> BinaryTree<elemType>::operator=(const BinaryTree& rhs){
+    if(this!=&rhs){
+        clear();
+        copy(_root,rhs._root);
+        return *this;
+    }
+}
+
+
+
+int main(int argc,char** argv){
+    
     return 0;
 }

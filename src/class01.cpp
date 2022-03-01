efb82f102080a09192303b3489f737afb2f94015
Chuancey <37216768+MiChuancey@users.noreply.github.com>
2022-03-01 14:39:12 +0800

    learn basic oop

---
+++ src/class01.cpp
@@ -0,0 +1,49 @@
+#include "class01.h"
+
+Sales_data::Sales_data(std::istream& is) {
+    // double price = 0;
+    // is >> bookno >> units_sold >> price;
+    // revenue = price * units_sold;
+    read(is, *this);
+}
+
+Sales_data& Sales_data::combine(const Sales_data& item) {
+    units_sold += item.units_sold;
+    revenue += item.revenue;
+    return *this;
+}
+
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+std::istream& read(std::istream& is, Sales_data& item) {
+    double price = 0;
+    is >> item.bookno >> item.units_sold >> price;
+    return is;
+}
+
+std::ostream& print(std::ostream& os, const Sales_data& item) {
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue
+       << item.avg_price();
+    return os;
+}
+
+/*******************/
+void Account::rate(double rate) {
+    interstRate = rate;
+}
+
+double Account::initRate() {
+    return interstRate = 0;
+}
+
+double Account::interstRate = initRate();
+
+int main(int argc, char* argv[]) {
+    std::vector<double> verc(10);
+    std::vector<int>::value_type x = 10;
+    return 0;
+}
---
+++ src/class01.h
@@ -0,0 +1,60 @@
+#ifndef __CLASS01_H__
+#define __CLASS01_H__
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Sales_data {
+    friend Sales_data add(const Sales_data&, const Sales_data&);
+    friend std::istream& read(std::istream&, Sales_data&);
+    friend std::ostream& print(std::ostream&, const Sales_data&);
+
+   public:
+    Sales_data() = default;
+    Sales_data(const std::string& s, unsigned n, double p)
+        : bookno(s), units_sold(n), revenue(p * n) {}
+    Sales_data(const std::string& s) : bookno(s) {}
+    Sales_data(std::istream&);
+    std::string isbn() const { return bookno; }
+    Sales_data& combine(const Sales_data&);
+
+   private:
+    std::string bookno;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+    double avg_price() const { return units_sold ? revenue / units_sold : 0; }
+};
+
+Sales_data add(const Sales_data&, const Sales_data&);
+std::istream& read(std::istream&, Sales_data&);
+std::ostream& print(std::ostream&, const Sales_data&);
+
+// Account类
+class Account {
+   public:
+    Account() = default;
+    void calculate() { amount += amount * interstRate; }
+    static double rate() { return interstRate; }
+    static void rate(double);
+
+   private:
+    std::string owner;
+    double amount = 0;
+    // 静态成员不在类内进行初始化
+    static double interstRate;
+    static double initRate();
+};
+
+class EX {
+   public:
+    static constexpr double rate = 1.0;
+    static const int vecsize = 10;
+    static std::vector<double> vec;
+    static char test[vecsize];
+};
+// double EX::rate = 2.0;
+std::vector<double> EX::vec(EX::vecsize);
+
+#endif
